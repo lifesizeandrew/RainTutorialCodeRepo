cd33684567ba526afd02d82cff75ae7f9c9679ba
Andrew Cline <lifesizeandrew@gmail.com>
2013-05-26 11:29:59 -0700

    Episode 50

--- src/net/lifesizedesign/rain/graphics/Screen.java
+++ src/net/lifesizedesign/rain/graphics/Screen.java
@@ -2,6 +2,7 @@ package net.lifesizedesign.rain.graphics;
 
 import java.util.Random;
 
+import net.lifesizedesign.rain.entity.mob.Player;
 import net.lifesizedesign.rain.level.tile.Tile;
 
 public class Screen {
@@ -41,8 +42,27 @@ public class Screen {
 			for (int x = 0; x < tile.sprite.SIZE; x++) {
 				int xa = x + xp;
 				if (xa < -tile.sprite.SIZE || xa >= width || ya < 0 || ya >= height) break;
-				if(xa<0) xa =0;
+				if (xa < 0) xa = 0;
 				pixels[xa + ya * width] = tile.sprite.pixels[x + y * tile.sprite.SIZE];
+			}
+		}
+	}
+
+	public void renderPlayer(int xp, int yp, Sprite sprite, int flip) {
+		xp -= xOffset;
+		yp -= yOffset;
+		for (int y = 0; y < 32; y++) {
+			int ya = y + yp;
+			int ys = y;
+			if (flip == 2 || flip == 3) ys = 31 - y;
+			for (int x = 0; x < 32; x++) {
+				int xa = x + xp;
+				int xs = x;
+				if (flip == 1 || flip == 3) xs = 31 - x;
+				if (xa < -32 || xa >= width || ya < 0 || ya >= height) break;
+				if (xa < 0) xa = 0;
+				int col = sprite.pixels[xs + ys * 32];
+				if (col != 0xffff00ff) pixels[xa + ya * width] = col;
 			}
 		}
 	}
@@ -51,5 +71,5 @@ public class Screen {
 		this.xOffset = xOffset;
 		this.yOffset = yOffset;
 	}
-	
+
 }
